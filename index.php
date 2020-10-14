a1ab07191efcfa54eacc7cd241da59c35e845d92
zlipknot <zlipknot.misha@gmail.com>
2020-10-15 01:33:17 +0300

    add codesniffer

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
-echo 'Hello, Hexlet!';
+require __DIR__ . '/vendor/autoload.php';
+
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
---
+++ src/Runner.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hexlet\Php\Runner;
+
+use Tightenco\Support\Collection;
+
+function run()
+{
+    $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
+        return strtoupper($name);
+    });
+
+    return $collection;
+}
